ad8948cb5340afabf5336ca2189691bca4cb08d5
Starmaker2021 <2968722911@qq.com>
2023-03-23 13:48:12 +0000

    3-23

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv_dynamic = TextView(this)
+        tv_dynamic.textSize = 20f
+        tv_dynamic.text = "安卓就尼玛离谱"
+//创建一个按钮
+        val mBtnTextView = findViewById<Button>(R.id.btn_textview)
+
+
+        val tecx = findViewById<LinearLayout>(R.id.ll_main_layout)
+        tecx.addView(tv_dynamic);
+
     }
 }
